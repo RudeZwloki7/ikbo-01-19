45af2b01b840b71b9fecd4fe56299477f191ea3b
Simon <rudezwloki7@gmail.com>
2020-09-15 09:22:06 +0300

    Лабораторная работа №1

---
+++ lab1 - Anikeev/src/com/company/Task2.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Task2 {
+    public void printArgs (String[] args) {
+        for (String str : args) {
+            System.out.println(str);
+        }
+    }
+}
+
